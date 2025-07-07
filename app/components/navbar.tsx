f078dc7c23aab04308ffbb4e1c23b560ee222289
Igor Teixeira <igorteixeira354@gmail.com>
2025-07-07 14:01:44 +0100

    feat: remove flowbite

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,27 +1,38 @@
-import { Navbar as Header, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function Navbar() {
-
     return (
-        <Header fluid>
-            <NavbarBrand as={Link} href="https://flowbite-react.com">
-                <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">REST<span className="text-blue-600">Explorer</span></span>
-            </NavbarBrand>
-            <NavbarToggle />
-            <NavbarCollapse>
-                <NavbarLink href="#" active>
-                    Home
-                </NavbarLink>
-                <NavbarLink as={Link} href="#">
-                    About
-                </NavbarLink>
-                <NavbarLink href="#">Countries</NavbarLink>
-                <NavbarLink href="#">Login</NavbarLink>
-
-            </NavbarCollapse>
-        </Header>
-
-    )
+        <header className="w-full px-8 text-gray-700 bg-white shadow-sm">
+            <div className="container flex flex-col md:flex-row items-center justify-between py-5 mx-auto max-w-7xl">
+                <div className="flex flex-col md:flex-row items-center">
+                    <NavLink to="/" className="flex items-center mb-5 md:mb-0">
+                        <span className="text-xl font-black text-gray-900 select-none">
+                            REST<span className="text-indigo-600">Explorer</span>
+                        </span>
+                    </NavLink>
+                    <nav className="flex flex-wrap items-center ml-0 md:ml-8 md:border-l md:pl-8">
+                        <NavLink
+                            to="/"
+                            end
+                            className="mr-5 font-medium text-gray-600 hover:text-gray-900"
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            to="/countries"
+                            className="mr-5 font-medium text-gray-600 hover:text-gray-900"
+                        >
+                            Countries
+                        </NavLink>
+                        <NavLink
+                            to="/about"
+                            className="mr-5 font-medium text-gray-600 hover:text-gray-900"
+                        >
+                            About
+                        </NavLink>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
 }
